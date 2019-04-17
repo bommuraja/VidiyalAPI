afd3d1d426a2e1efec97676f4093d1b3c04f1534
Bommuraja <rbommuraja@outlook.com>
2019-04-18 01:01:10 +0530

    t3

--- VidiyalAPI/App_Start/BundleConfig.cs
+++ VidiyalAPI/App_Start/BundleConfig.cs
@@ -22,6 +22,7 @@ namespace VidiyalAPI
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
         }
     }
 }
